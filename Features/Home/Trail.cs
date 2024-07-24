340b9d0b1d88fa7126d5deef067e02341fecaba7
Артур Пирожков <large000@yandex.ru>
2024-07-24 06:35:24 +0300

    реализовал процедуру фильтрации тропинок по длине маршрута

--- Features/Home/Trail.cs
+++ Features/Home/Trail.cs
@@ -1,24 +1,20 @@
-﻿namespace BlazingTrails.Client.Features.Home
+﻿namespace BlazingTrails.Client.Features.Home;
+
+public class Trail
 {
-    public class Trail
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = String.Empty;
-        public string Image { get; set; } = string.Empty;
-        public  string Location { get; set; } = string.Empty;
-        public int TimeInMinutes { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string Image { get; set; } = "";
+    public string Location { get; set; } = "";
+    public int TimeInMinutes { get; set; }
+    public string TimeFormatted => $"{TimeInMinutes / 60}h {TimeInMinutes % 60}m";
+    public int Length { get; set; }
+    public IEnumerable<RouteInstruction> Route { get; set; } = Array.Empty<RouteInstruction>();
+}
 
-        public string TimeFormatted => $"{TimeInMinutes / 60}h {TimeInMinutes % 60}m";
-        public int Length { get; set; }
-
-        public IEnumerable<RouteInstruction> Route { get; set; } = Array.Empty<RouteInstruction>();
-
-    }
-
-    public class RouteInstruction
-    {
-        public int Stage { get;set; }
-        public string Description { get; set; } = "";
-    }
+public class RouteInstruction
+{
+    public int Stage { get; set; }
+    public string Description { get; set; } = "";
 }
