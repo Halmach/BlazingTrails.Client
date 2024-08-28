986a9923df1cc8d27f40966ecffb3ccfedde09dd
Артур Пирожков <large000@yandex.ru>
2024-08-28 06:38:36 +0300

    Добавление маршрута в БД работает

---
+++ Features/ManageTrails/AddTrailHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Json;
+using MediatR;
+
+namespace BlazingTrails.Client.Features.ManageTrails
+{
+    // обработчики запросов реализуют интерфейс IRequestHandler<TRequest,TResponse>.
+    // TRequestHandler - это тип запроса, который обрабатывает обработчик.
+    // TResponse - тип ответа, который вернет обработчик
+    public class AddTrailHandler: IRequestHandler<AddTrailRequest,AddTrailRequest.Response>
+    {
+        private readonly HttpClient _httpClient;
+
+        // Внедряем экземпляр HttpClient и сохраняем
+        // в поле, которое будет использоваться для вызовов API
+        public AddTrailHandler(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        // Метод Handler определяется интерфейсом IRequestHandler и вызывается для обработки запроса MediatR
+        public async Task<AddTrailRequest.Response> Handle(AddTrailRequest request, CancellationToken cancellationToken)
+        {
+            var response = await _httpClient.PostAsJsonAsync(AddTrailRequest.RouteTemplate, request, cancellationToken);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var trailId = await response.Content.ReadFromJsonAsync<int>(cancellationToken:cancellationToken);
+                return new AddTrailRequest.Response(trailId);
+            }
+            else
+            {
+                return new AddTrailRequest.Response(-1);
+            }
+        }
+    }
+}
---
+++ Features/ManageTrails/AddTrailRequest.cs
@@ -0,0 +1,27 @@
+﻿using BlazingTrails.Client.Features.Home;
+using BlazingTrails.Shared.Features.ManageTrails;
+using FluentValidation;
+using MediatR;
+
+namespace BlazingTrails.Client.Features.ManageTrails
+{
+    public record AddTrailRequest(TrailDto Trail):IRequest<AddTrailRequest.Response>
+    {
+        // адрес конечной точки API запроса
+        public const string RouteTemplate = "/api/trails";
+
+        // вложенная запись определяет данные ответа на запрос
+        public record Response(int TrailId);
+    }
+
+    // Валидатор запроса.Он будет выполняться API, чтобы убедиться, что данные запроса корректны
+    public class AddTrailRequestValidator : AbstractValidator<AddTrailRequest>
+    {
+        public AddTrailRequestValidator()
+        {
+            // Опеределяет TrailValidator в качестве валидатора свойства Trail.
+            // Позволяет повторно использовать созданные нами ранее правила валидации
+            RuleFor(x => x.Trail).SetValidator(new TrailValidator());
+        }
+    }
+}
